222da1e22fadca4dc366ac5dd3c5289c1bab4078
Dominik Haas <shaodiese@gmail.com>
2020-09-01 11:32:26 +0200

    added vuetify plugin files

--- src/main.js
+++ src/main.js
@@ -1,4 +1,12 @@
-import { createApp } from 'vue'
+import Vue from 'vue'
 import App from './App.vue'
+import vuetify from './plugins/vuetify';
 
-createApp(App).mount('#app')
+Vue.config.productionTip = false
+
+new Vue({
+  el: '#app',
+  vuetify,
+  components: { App },
+  template: '<App/>',
+});
