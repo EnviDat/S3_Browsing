696f17c40fff0da7dc27ab654893ef2508482565
Dominik Haas <shaodiese@gmail.com>
2020-09-01 21:08:53 +0200

    basic TreeCard & TreeView implemetation

--- src/components/TreeView.vue
+++ src/components/TreeView.vue
@@ -1,7 +1,7 @@
 <template>
 
   <v-treeview v-model="tree"
-              :open="open"
+              :search="search"
               :items="items"
               activatable
               item-key="name"
@@ -12,11 +12,15 @@
       <v-icon v-if="!item.file">
         {{ open ? 'mdi-folder-open' : 'mdi-folder' }}
       </v-icon>
-
+        
       <v-icon v-else>
         {{ files[item.file] }}
       </v-icon>
     </template>
+
+    <!-- <template v-slot:label="{ item }">
+      {{ item.name }}
+    </template> -->
 
   </v-treeview>
 </template>
@@ -24,8 +28,22 @@
 <script>
   export default {
     name: 'TreeView',
+    props: {
+      items: Array,
+      search: String,
+      caseSensitive: Boolean,
+    },
+    computed: {
+    },
+    methods: {
+      filter() {
+        return this.caseSensitive
+          ? (item, search, textKey) => item[textKey].indexOf(search) > -1
+          : undefined;
+      },      
+    },
     data: () => ({
-      open: ['public'],
+      open: [],
       files: {
         html: 'mdi-language-html5',
         js: 'mdi-nodejs',
@@ -33,62 +51,64 @@
         md: 'mdi-markdown',
         pdf: 'file-pdf',
         png: 'mdi-file-image',
+        tiff: 'mdi-file-image',
+        tif: 'mdi-file-image',
         txt: 'mdi-file-document-outline',
         xls: 'mdi-file-excel',
       },
       tree: [],
-      items: [
-        {
-          name: '.git',
-        },
-        {
-          name: 'node_modules',
-        },
-        {
-          name: 'public',
-          children: [
-            {
-              name: 'static',
-              children: [{
-                name: 'logo.png',
-                file: 'png',
-              }],
-            },
-            {
-              name: 'favicon.ico',
-              file: 'png',
-            },
-            {
-              name: 'index.html',
-              file: 'html',
-            },
-          ],
-        },
-        {
-          name: '.gitignore',
-          file: 'txt',
-        },
-        {
-          name: 'babel.config.js',
-          file: 'js',
-        },
-        {
-          name: 'package.json',
-          file: 'json',
-        },
-        {
-          name: 'README.md',
-          file: 'md',
-        },
-        {
-          name: 'vue.config.js',
-          file: 'js',
-        },
-        {
-          name: 'yarn.lock',
-          file: 'txt',
-        },
-      ],
+      // items: [
+      //   {
+      //     name: '.git',
+      //   },
+      //   {
+      //     name: 'node_modules',
+      //   },
+      //   {
+      //     name: 'public',
+      //     children: [
+      //       {
+      //         name: 'static',
+      //         children: [{
+      //           name: 'logo.png',
+      //           file: 'png',
+      //         }],
+      //       },
+      //       {
+      //         name: 'favicon.ico',
+      //         file: 'png',
+      //       },
+      //       {
+      //         name: 'index.html',
+      //         file: 'html',
+      //       },
+      //     ],
+      //   },
+      //   {
+      //     name: '.gitignore',
+      //     file: 'txt',
+      //   },
+      //   {
+      //     name: 'babel.config.js',
+      //     file: 'js',
+      //   },
+      //   {
+      //     name: 'package.json',
+      //     file: 'json',
+      //   },
+      //   {
+      //     name: 'README.md',
+      //     file: 'md',
+      //   },
+      //   {
+      //     name: 'vue.config.js',
+      //     file: 'js',
+      //   },
+      //   {
+      //     name: 'yarn.lock',
+      //     file: 'txt',
+      //   },
+      // ],
     }),
   };
 </script>
