6cc5d1a498e87531916916c95c9c4023b3ef85f8
Dominik <shaodiese@gmail.com>
2020-09-03 16:53:32 +0200

    downgraded vue-cli to 4.4.6 and add core-js polyfills

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,10 @@
 module.exports = {
   presets: [
-    '@vue/cli-plugin-babel/preset',
+    ['@babel/preset-env',
+      {
+        targets: { node: 'current' },
+        useBuiltIns: 'usage',
+        corejs: '3.6',
+      }],
   ],
 };
