65adb2ba5d2ff494d9e73f533ae10365dbe91482
Dominik Haas <shaodiese@gmail.com>
2020-09-10 08:53:27 +0200

    readded the different image icons;

--- src/components/TreeView.vue
+++ src/components/TreeView.vue
@@ -1,33 +1,15 @@
 <template>
-
-        <v-treeview :items="items"
-                    :load-children="fetchSubkeys"
-                    :open.sync="open"
-                    color="warning" >
-                    <!-- :active.sync="active"
-                    activatable -->
-
-          <template v-slot:prepend="{ item }">
-          <!-- <template v-slot:prepend="{ item, active }"> -->
-            <!-- {{ active ? 'active' : 'inactive' }} -->
-            <v-icon v-if="item && item.children && item.children.length > 0">mdi-folder</v-icon>
-            <v-icon v-if="item && !item.children || item.children.length <= 0">mdi-file</v-icon>
-          </template>
-
-        </v-treeview>
-
-  <!-- <v-treeview v-model="tree"
-              :active.sync="active"
-              :search="search"
-              :items="items"
+  <v-treeview :items="items"
               :load-children="fetchSubkeys"
+              :open.sync="open"
+              :search="search"
               item-key="name"
               open-on-click >
 
     <template v-slot:prepend="{ item, open }" >
 
-      <v-icon v-if="item.file">
-        {{ files[item.file] }}
+      <v-icon v-if="item.isFile">
+        {{ fileExtentions[item.fileExt] }}
       </v-icon>
         
       <v-icon v-else>
@@ -39,31 +21,31 @@
 
       <v-row no-gutters
               align="center">
-        <v-col>
+        <v-col class="pl-2 pr-4 shrink">
           {{ item.name }}
         </v-col>
 
-        <v-col v-if="item.file"
+        <v-col v-if="item.isFile"
                 class="shrink px-1" >
 
           <IconButton icon="mdi-content-copy"
                       tooltipText="Copy Link"
-                      @click="catchCopyClick(item.name)" />
+                      @click="catchCopyClick(item.fileUrl)" />
         </v-col>
-        <v-col v-if="item.file"
+        <v-col v-if="item.isFile"
                 class="shrink px-1" >
 
           <IconButton icon="mdi-cloud-download"
                       tooltipText="Download file"
-                      :url="item.url" />
+                      :url="item.fileUrl" />
         </v-col>
 
-        <v-col v-if="item.file"
+        <v-col v-if="item.isFile"
                 class="shrink px-1 caption" >
           {{ item.size }}
         </v-col>
 
-        <v-col v-if="!item.file"
+        <v-col v-if="!item.isFile"
                 cols="1"
                 class="shrink"  >
           <v-badge color="primary"
@@ -74,7 +56,7 @@
       </v-row>
     </template>
 
-  </v-treeview> -->
+  </v-treeview>
 </template>
 
 <script>
@@ -88,8 +70,6 @@ import {
 } from '@/store/mutationsConsts';
 
 import IconButton from './IconButton';
-
-// const pause = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
 export default {
   name: 'TreeView',
@@ -106,26 +86,19 @@ export default {
       'contentMap',
     ]),
     ...mapState([
-      // 'configLoading',
-      // 'configError',
       'content',
       'contentLoading',
-      // 'contentError',
     ]),
+    items() {
+      return this.values ? this.values : [];
+    },
     values() {
       // return this.contentMap ? this.contentMap.values() : null;
       return this.contentMap ? Object.values(this.contentMap) : null;
     },
-    items() {
-      // return this.values ? [...this.values] : [];
-      return this.values ? this.values : [];
-    },
   },
   methods: {
     extractUrlParameters() {
-      // this.urlPrefix = 'chelsa/chelsa_V1/chelsa_cruts/prec/';
-      // return;
-      // eslint-disable-next-line no-unreachable
       let params = this.$route.query;
 
       this.urlPrefix = params?.prefix || null;
@@ -158,27 +131,22 @@ export default {
       });
     },
     async fetchSubkeys(item) {
-      // console.log(`started fetchSubkeys ${item.directory}`);
+      // console.log({ item });
+
+      if (item.isFile) {
+        return;
+      }
 
       const prefix = item.directory;
-      // await pause(1500);
+
       await this.$store.dispatch(GET_S3_CONTENT, {
         url: this.contentUrl,
         prefix,
       });
 
-      // console.log('finished fetchSubkeys');
+      item.open = true;
     },
   },
-  // watch: {
-  //   configLoading() {
-  //       console.log(`got configLoading ${this.configLoading}`);
-
-  //     if (!this.configLoading && this.contentUrl) {
-  //       this.loadS3Content(this.contentUrl, '', '/');
-  //     }
-  //   },
-  // },
   data: () => ({
     copySnackText: 'Url copied to clipboard',
     active: [],
@@ -191,13 +159,15 @@ export default {
       md: 'mdi-markdown',
       pdf: 'file-pdf',
       png: 'mdi-file-image',
+      jpeg: 'mdi-file-image',
+      jpg: 'mdi-file-image',
       tiff: 'mdi-file-image',
       tif: 'mdi-file-image',
       txt: 'mdi-file-document-outline',
       xls: 'mdi-file-excel',
       nc: 'mdi-file',
+      zip: 'mdi-zip-box',
     },
-    tree: [],
   }),
   components: {
     IconButton,
